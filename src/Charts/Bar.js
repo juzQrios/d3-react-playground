0e5ff2d53e88ab1912346f158d0935bcbe66cac9
juzQrios <dj30c.1@gmail.com>
2021-03-16 22:13:01 +0530

    Configure d3 to work with react

--- src/Charts/Bar.js
+++ src/Charts/Bar.js
@@ -1,11 +1,26 @@
-const Bar = (props) => {
-  return (
-    <div className="Bar">
-      <header>
-        <h1>Bar Chart</h1>
-      </header>
-    </div>
-  );
+import * as d3 from 'd3';
+import React from 'react';
+
+class Bar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.message = React.createRef();
+  }
+
+  componentDidMount() {
+    d3.select(this.message.current).html('Hello World from D3');
+  }
+
+  render() {
+    return (
+      <div className="Bar">
+        <header>
+          <h1>Bar Chart</h1>
+          <div className="message" ref={this.message}></div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default Bar;
