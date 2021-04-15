f897904102fcc4730fdb5f5dbd601acc3a78e6df
juzQrios <dj30c.1@gmail.com>
2021-04-15 21:33:05 +0530

    Add code to import CSV file

--- src/charts/bar_charts/Bar.js
+++ src/charts/bar_charts/Bar.js
@@ -5,13 +5,17 @@ const Bar = (props) => {
     <PageTemplate>
       <div className="references">
         <div>
-          <a href="https://observablehq.com/@d3/lets-make-a-bar-chart" target="_blank">Let's make a bar chart @observable</a>
+          <a href="https://observablehq.com/@d3/lets-make-a-bar-chart" target="_blank">ObservableHQ: Let's make a bar chart</a>
         </div>
         <div>
           <a href="https://medium.com/@louisemoxy/a-simple-way-to-make-d3-js-charts-svgs-responsive-7afb04bc2e4b" target="_blank">Simple way to make d3js charts responsive</a>
         </div>
         <div>
-          <a href="https://observablehq.com/@d3/lets-make-a-bar-chart/3" target="_blank">Let's make bar chart, Part 3 @observable</a>
+          <a href="https://observablehq.com/@d3/lets-make-a-bar-chart/3" target="_blank">ObservableHQ: Let's make bar chart, Part 3</a>
+        </div>
+        <div>
+          <a href="https://stackoverflow.com/questions/51258615/reactjs-d3-parse-local-csv-file-and-passing-it-to-state-with-d3-request" target="_blank">Stackoverflow: ReactJS + D3: Parse local CSV file and passing it to state with d3-request
+</a>
         </div>
       </div>
     </PageTemplate>
--- src/charts/bar_charts/Bar2/Bar2.js
+++ src/charts/bar_charts/Bar2/Bar2.js
@@ -2,14 +2,16 @@ import * as d3 from 'd3';
 import React from 'react';
 import './Bar2.css';
 import PageTemplate from '../../../utils/PageTemplate';
+import data from './data.csv';
 
 class Bar2 extends React.Component {
   constructor(props) {
     super(props);
     this.chart = React.createRef();
   }
-
+  
   componentDidMount() {
+    d3.csv(data, (data) => { console.log(data) })
   }
 
   render() {
